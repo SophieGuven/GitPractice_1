27612ceec2c59b6fe45a1d5f5cf6fb36f3fc3f91
SophieGuven <teslimesofi@gmail.com>
2021-07-26 12:53:01 -0400

    testcase part is done

--- src/day1/ResetPractice.java
+++ src/day1/ResetPractice.java
@@ -4,4 +4,8 @@ public class ResetPractice {
     //test case part 1 is done
     //test case part 1 is done
     //test case part 1 is done
+    //test case part 1 is done
+    //test case part 5 is done
+    //mixed|soft reset remove the commit from history but keep the local changes
+    // did part 4 and 5 part I am done in one commit
 }
