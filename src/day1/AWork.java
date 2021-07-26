5b71f8c408f901c4d78fd6861deab3207efc31ec
SophieGuven <teslimesofi@gmail.com>
2021-07-26 15:33:20 -0400

    feature a work has started

---
+++ src/day1/AWork.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class AWork {
+    public static void main(String[] args) {
+
+    }
+}
--- src/day1/ResetPractice.java
+++ src/day1/ResetPractice.java
@@ -1,6 +1,7 @@
 package day1;
 
 public class ResetPractice {
+
     //test case part 1 is done
     //test case part 1 is done
     //test case part 1 is done
